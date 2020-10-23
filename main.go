41f1aca237895b3a2719d3934656b58c63d21839
Dordzhi <isinov1995@gmail.com>
2020-10-23 03:05:25 +0300

    Split Strings

--- main.go
+++ main.go
@@ -3,40 +3,26 @@ package main
 import "fmt"
 
 func main() {
-
-	//fmt.Println(maxIntExcept([]int{6,5,83,5,3,18}))
-	fmt.Println(TwoOldestAges([]int{61, 11, 33, 79, 81, 27, 79, 83, 9, 95}))
-}
-func TwoOldestAges(ages []int) [2]int {
-	var res [2]int
-	slice := make([]int, len(ages), len(ages))
-	slice = ages
-	maxOne := maxIntExcept(slice)
-
-	slice = remove(slice, maxOne[1])
-
-	maxTwo := maxIntExcept(slice)
-	res[0] = maxTwo[0]
-	res[1] = maxOne[0]
-	return res
+	Solution("abc")                 //should return ["ab", "c_"]
+	fmt.Println(Solution("abcdef")) //should return ["ab", "cd", "ef"]
 }
 
-func remove(slice []int, s int) []int {
-	return append(slice[:s], slice[s+1:]...)
-}
-
-func maxIntExcept(slc []int) [2]int {
-	var max int
-	var maxKey int
-	max = slc[0]
-	for k, v := range slc {
-
-		if v > max {
-
-			max = v
-			maxKey = k
-
-		}
+func Solution(str string) []string {
+	res := make([]string, 0, len(str))
+	pair := ""
+	cnt := len(str)
+	if cnt%2 > 0 {
+		str = str + "_"
 	}
-	return [2]int{max, maxKey}
+
+	for i := 0; i < cnt; {
+		fmt.Println(i)
+		if i+2 <= len(str) {
+			pair = str[i : i+2]
+			fmt.Println(pair)
+		}
+		res = append(res, pair)
+		i = i + 2
+	}
+	return res
 }
