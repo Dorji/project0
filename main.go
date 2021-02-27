8c59ca6c95a01a92fa1741d62909053a2505d9fa
isinov <isinov1995@gmail.com>
2021-02-27 19:10:06 +0300

    bubble sort

--- main.go
+++ main.go
@@ -2,44 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strconv"
-	"unicode/utf8"
 )
 
 func main() {
-	fmt.Println(utf8.RuneCountInString(strconv.Itoa(144444)))
-	fmt.Printf("sum is: %v", DigitalRoot(195))
-}
 
-func DigitalRoot(n int) int {
-	var sum int = 0
-	var slc = IntToSlice(n)
-	for _, v := range slc {
-		sum += v
-	}
-	if sum/10 > 0 {
-		sum = DigitalRoot(sum)
-	}
+	var mass = [10]int{2, 3, 42, 5, 6, 8, 9, 333, 55, 6}
 
-	return sum
-}
-func IntToSlice(n int) []int {
-	var number = n
-	var lenN = utf8.RuneCountInString(strconv.Itoa(n))
-	res := make([]int, 0, lenN)
-	var elem0 int
-	var elem1 int
-
-	for i := 1; i < lenN+1; i++ {
-		elem0 = number % int(math.Pow(float64(10), float64(i)))
-		if i != 1 {
-
-			elem1 = elem0 / int(math.Pow(float64(10), float64(i-1)))
-		} else {
-			elem1 = elem0
+	var elem int
+	for k := 0; k <= 9; k++ {
+		for i := 1; i <= 9; i++ {
+			if mass[i] < mass[i-1] {
+				elem = mass[i]
+				mass[i] = mass[i-1]
+				mass[i-1] = elem
+			}
 		}
-		res = append(res, elem1)
 	}
-	return res
+	fmt.Println(mass)
+
 }
