9ed957a588fda77548a0247c9a0749b189860e0e
isinov <isinov1995@gmail.com>
2021-02-28 23:00:29 +0300

    quick sort

--- main.go
+++ main.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
 
-	var mass = [10]int{2, 3, 42, 5, 6, 8, 9, 333, 55, 6}
+	var mass = []int{2, 3, 42, 5, 6, 8, 9, 333, 55, 6}
 
-	var elem int
-	for k := 0; k <= 9; k++ {
-		for i := 1; i <= 9; i++ {
-			if mass[i] < mass[i-1] {
-				elem = mass[i]
-				mass[i] = mass[i-1]
-				mass[i-1] = elem
-			}
+	fmt.Println(quicksort(mass))
+
+}
+
+func quicksort(mass []int) []int {
+	if len(mass) < 2 {
+		return mass
+	}
+	left := 0
+	right := len(mass) - 1
+
+	pivot := rand.Int() % len(mass)
+
+	tmp := 0
+	tmp = mass[pivot]
+	mass[pivot] = mass[right]
+	mass[right] = tmp
+
+	for i, _ := range mass {
+		if mass[i] < mass[right] {
+			tmp = mass[left]
+			mass[left] = mass[i]
+			mass[i] = tmp
+			left++
 		}
 	}
-	fmt.Println(mass)
+	tmp = mass[left]
+	mass[left] = mass[right]
+	mass[right] = tmp
 
+	quicksort(mass[:left])
+	quicksort(mass[left+1:])
+	return mass
 }
