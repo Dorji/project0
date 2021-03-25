6105e5f794fa8f36c8a06f18e12f87c07d7a7fd0
isinov <isinov1995@gmail.com>
2021-03-26 02:30:28 +0300

    delete from slice

--- main.go
+++ main.go
@@ -2,70 +2,13 @@ package main
 
 import "fmt"
 
-const (
-	// Vert - кол-во вершин
-	Vert = 7
-)
-
-var used = make(map[int]bool)
-var graph = [Vert][]int{
-	{1, 2},
-	{0, 2},
-	{0, 1, 3, 4, 5},
-	{2},
-	{2, 6},
-	{2, 6},
-	{4, 5},
-}
-
 func main() {
-	fmt.Println(dfs(3, 6))
-	fmt.Println(bfs(5))
+	var arr = []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(arr)
+	arr = deleteElem(arr, 2) //не забывай добавлять присваивание
+	fmt.Println(arr)
 }
+func deleteElem(arr []int, index int) []int {
+	return append(arr[:index], arr[index+1:]...)
 
-func dfs(start int, needle int) int {
-	used[start] = true
-	for _, n := range graph {
-		for _, v := range n {
-			if _, ok := used[v]; !ok {
-				if v == needle {
-					return v
-				}
-				dfs(v, needle)
-			}
-		}
-	}
-	return -1
-}
-
-func bfs(needle int) int {
-	graph := [Vert][]int{
-		{1, 2},
-		{0, 2},
-		{0, 1, 3, 4, 5},
-		{2},
-		{2, 6},
-		{2, 6},
-		{4, 5},
-	}
-	start := 2
-	q := make([][]int, len(graph))[len(graph):]
-	q = append(q, graph[start])
-
-	used := make(map[int]bool, len(graph))
-	used = map[int]bool{start: true}
-
-	for len(q) > 0 {
-		for _, key := range q[len(q)-1] {
-			if _, ok := used[key]; !ok {
-				q = append(q, graph[key])
-			}
-			if key == needle {
-				return key
-			}
-			used[key] = true
-		}
-		q = q[:len(q)-1]
-	}
-	return -1
 }
