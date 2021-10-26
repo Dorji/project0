976a56e7f4157be06e3e2a9cde15fc6097982408
isinov <disinov@syntes.io>
2021-10-26 10:47:15 +0300

    tinkoff test first

--- main.go
+++ main.go
@@ -2,26 +2,19 @@ package main
 
 import "fmt"
 
-func ArrayRotation(arr []int) string {
-
-	start := arr[0]
-	res := ""
-	arr = append(arr[start:], arr[:start]...)
-
-	for _, v := range arr {
-		res = fmt.Sprintf("%s%v", res, v)
-	}
-
-	return res
-
-}
 func main() {
 
 	// do not modify below here, readline is our function
 	// that properly reads in the input for you
-	//
 
-	var arr = []int{2, 3, 4, 1, 6, 10}
-	fmt.Println(ArrayRotation(arr))
+	var arr []*int
+	var arrSrc = []int{2, 3, 4, 1, 6, 10}
 
+	for k := range arrSrc {
+		arr = append(arr, &arrSrc[k])
+	}
+
+	for _, v := range arr {
+		fmt.Println(*v)
+	}
 }
