0e644cb6be55063329ca7817e44625fa50069ed9
Dordzhi <isinov1995@gmail.com>
2025-03-21 17:34:15 +0300

    loops

---
+++ patterns/slices/loops.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+// дан целочисленный массив и целое число (target). вернуть индексы двух чисел, сумма которых равна target
+// Input: nums = [3,9,11,15], target = 14
+// Output: [0,2]
+
+func loops1() {
+
+	nums := []int{3, 9, 11, 15}
+	target := 14
+
+	numsMap := make(map[int]int)
+
+	for k := range nums {
+		numsMap[nums[k]] = k
+	}
+LOOP:
+	for k := range nums {
+		if _, ok := numsMap[target-nums[k]]; ok {
+			fmt.Println(k, numsMap[target-nums[k]])
+			break LOOP
+		}
+	}
+
+}
+
+//1,2,3,9,11,13,14,15
+//14 0
+
+// O(n^2)
+func loops2() {
+
+	nums := []int{9, 11, 3, 15}
+	target := 14
+	//mem:= make(map[int]int)
+
+LOOP:
+	for k := range nums {
+		for key := range nums {
+			if key == k {
+				continue
+			}
+			if nums[k]+nums[key] == target {
+				fmt.Println(k, key)
+				break LOOP
+			}
+		}
+
+	}
+
+}
