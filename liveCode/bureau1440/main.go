98990742f88c779ea50618abf45934555a0e9e9d
Dordzhi <isinov1995@gmail.com>
2025-04-03 00:40:12 +0300

    livecode

--- liveCode/bureau1440/main.go
+++ liveCode/bureau1440/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
-    "context"
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func init() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 // Есть функция, работающая неопределённо долго и возвращающая число.
 // Её тело нельзя изменять (представим, что внутри сетевой запрос).
 func unpredictableFunc() int64 {
-    rnd := rand.Int63n(5000)
-    time.Sleep(time.Duration(rnd) * time.Millisecond)
-
-    return rnd
+	rnd := rand.Int63n(5000)
+	time.Sleep(time.Duration(rnd) * time.Millisecond)
+	return rnd
 }
 
 // Нужно изменить функцию обёртку, которая будет работать с заданным таймаутом (например, 1 секунду).
@@ -26,31 +25,36 @@ func unpredictableFunc() int64 {
 //
 // Дополнительно нужно измерить, сколько выполнялась эта функция (просто вывести в лог).
 // Сигнатуру функцию обёртки менять можно.
-func predictableFunc(ctx context.Context) (int64,error) {
-    ch:= make(chan int64,1)
-    defer close(ch)
-    
-    
-    go func(){
-         ch<-unpredictableFunc()
-    }()
+func predictableFunc(ctx context.Context) (int64, error) {
+	start := time.Now()
+	ch := make(chan int64, 1)
 
-    select {
-        case <-ctx.Done():
-            return 0,fmt.Errorf("MyError")
-        case res:=<-ch:
-            return res,nil
-    }
-    
-     
-     return 0,fmt.Errorf("MyError")
+	go func() {
+		ch <- unpredictableFunc()
+	}()
+
+	select {
+	case <-ctx.Done():
+		duration := time.Since(start)
+		fmt.Printf("Function execution timed out after %v\n", duration)
+		return 0, ctx.Err()
+	case res := <-ch:
+		duration := time.Since(start)
+		fmt.Printf("Function completed successfully in %v\n", duration)
+		return res, nil
+	}
 }
 
 func main() {
-    fmt.Println("started")
-    
-    ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	fmt.Println("started")
 
-    fmt.Println(predictableFunc(ctx))
-    cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	res, err := predictableFunc(ctx)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Result:", res)
+	}
 }
