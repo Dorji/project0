d62a186615be3d68ddbbf52b1a0428a1e324d4da
Dordzhi Isinov <disinov@anykey.pro>
2026-05-18 16:53:12 +0300

    simple wp

--- patterns/workingpool/main.go
+++ patterns/workingpool/main.go
@@ -6,42 +6,46 @@ import (
 	"time"
 )
 
+
 func main() {
 	WorkerPool(5)
 }
-
-//Реализовать worker pool. Есть 10 задач (функций), каждая засыпает на 1 сек и выводит номер воркера, который эту задачу исполнил. Количество воркеров задается при запуске.
-
-func WorkerPool(num int) {
-	chanWork := make(chan *Work, num)
-
+func WorkerPool(numWorkers int) {
+	// Вариант 1: Буферизированный канал
+	chanWork := make(chan *Work, 10) // буфер на все задачи
+	
+	// Вариант 2: Отправка в отдельной горутине
+	// chanWork := make(chan *Work)
+	
 	wg := &sync.WaitGroup{}
-	for i := 0; i < num; i++ {
+	
+	// Запускаем воркеры
+	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go worker(wg, chanWork)
+		go worker(i+1, wg, chanWork) // i+1 чтобы нумерация была с 1
 	}
-
+	
+	// Отправляем задачи
 	for i := 0; i < 10; i++ {
-		chanWork <- &Work{number: i}
+		chanWork <- &Work{number: i + 1}
 	}
-
-	close(chanWork)
-	wg.Wait()
+	
+	close(chanWork) // Сигнал воркерам что задач больше не будет
+	wg.Wait()       // Ждем завершения всех воркеров
 }
 
 type Work struct {
 	number int
 }
 
-func (w *Work) StartWork() {
+func (w *Work) StartWork(workerID int) {
 	time.Sleep(time.Second)
-	fmt.Println(w.number)
+	fmt.Printf("Воркер %d выполнил задачу %d\n", workerID, w.number)
 }
 
-func worker(wg *sync.WaitGroup, chanWork chan *Work) {
+func worker(id int, wg *sync.WaitGroup, chanWork <-chan *Work) {
 	defer wg.Done()
-	for val := range chanWork {
-		val.StartWork()
+	for work := range chanWork {
+		work.StartWork(id)
 	}
-
 }
