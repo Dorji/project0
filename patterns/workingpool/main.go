c154c0a82e9ab86f4feb734102e7b08ea53da774
Dordzhi Isinov <disinov@anykey.pro>
2026-05-18 16:10:18 +0300

    lint

--- patterns/workingpool/main.go
+++ patterns/workingpool/main.go
@@ -6,42 +6,42 @@ import (
 	"time"
 )
 
-
-func main(){
+func main() {
 	WorkerPool(5)
-} 
+}
+
 //Реализовать worker pool. Есть 10 задач (функций), каждая засыпает на 1 сек и выводит номер воркера, который эту задачу исполнил. Количество воркеров задается при запуске.
 
-func WorkerPool(num int){
-	chanWork:=make(chan *Work ,0)
-	// ctx,cancel:=context.WithCancel(context.Background())
-	wg:= &sync.WaitGroup{}
-	for i:=0;i<num;i++{
+func WorkerPool(num int) {
+	chanWork := make(chan *Work, num)
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < num; i++ {
 		wg.Add(1)
-		go worker(wg, num, chanWork )
+		go worker(wg, chanWork)
 	}
 
-	
-	for i:=0;i<10;i++{
-		chanWork<-&Work{ number:i}
+	for i := 0; i < 10; i++ {
+		chanWork <- &Work{number: i}
 	}
-	
+
 	close(chanWork)
 	wg.Wait()
 }
 
-type Work struct{
+type Work struct {
 	number int
 }
 
-func (w *Work)StartWork(){
+func (w *Work) StartWork() {
 	time.Sleep(time.Second)
 	fmt.Println(w.number)
 }
-func worker( wg *sync.WaitGroup, num int, chanWork chan *Work){
+
+func worker(wg *sync.WaitGroup, chanWork chan *Work) {
 	defer wg.Done()
-	for val:=range chanWork{
+	for val := range chanWork {
 		val.StartWork()
 	}
-		
+
 }
