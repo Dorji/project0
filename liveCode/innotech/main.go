72451286c572f27d5bc5f10b4081caf1df7e161e
Dordzhi Isinov <disinov@anykey.pro>
2026-05-18 18:15:13 +0300

    ref

--- liveCode/innotech/main.go
+++ liveCode/innotech/main.go
@@ -1,81 +1,70 @@
 /*
-
 Слить N каналов в один
 Даны n каналов типа chan int. Надо написать функцию, которая смерджит все данные из этих каналов в один и вернет его.
 Мы хотим, чтобы результат работы функции выглядел примерно так:
-for num := range joinChannels(a, b, c) {
-     fmt.Println(num)
-}
 
+	for num := range joinChannels(a, b, c) {
+	     fmt.Println(num)
+	}
 */
 package main
 
 import (
-    // "context"
-    "fmt"
-    "sync"
+	// "context"
+	"fmt"
+	"sync"
 )
 
 func main() {
 
-    a := make(chan int)
-    b := make(chan int)
-    c := make(chan int)
+	a := make(chan int)
+	b := make(chan int)
+	c := make(chan int)
 
-    go func() {
-        defer close(a)
-        for i := 0; i < 5; i++ {
-            a <- i
-        }
-    }()
+	go func() {
+		defer close(a)
+		for i := 0; i < 5; i++ {
+			a <- i
+		}
+	}()
 
-    go func() {
-        defer close(b)
-        for i := 5; i < 10; i++ {
-            b <- i
-        }
-    }()
+	go func() {
+		defer close(b)
+		for i := 5; i < 10; i++ {
+			b <- i
+		}
+	}()
 
-    go func() {
-        defer close(c)
-        for i := 10; i < 15; i++ {
-            c <- i
-        }
-    }()
-    
-    out := joinChannels(a, b, c)
-    for num := range out {
-        fmt.Println(num)
-    }
+	go func() {
+		defer close(c)
+		for i := 10; i < 15; i++ {
+			c <- i
+		}
+	}()
+
+	out := joinChannels(a, b, c)
+	for num := range out {
+		fmt.Println(num)
+	}
 }
 
-func joinChannels(chs ...<- chan int) chan int{
-     res:=make(chan int) 
-     
-    wg:=&sync.WaitGroup{} 
-    
-        for _,v := range chs{
-            wg.Add(1)
-            go func(){
-               defer wg.Done()
-               for{
-                    if val,ok := <-v; ok{
-                         res<-val
-                    }else{
-                         return
-                    }
-               }
-            }()
-        } 
-    
-    go func(){
-        wg.Wait()
-      close(res)
-    }()
-    
-    return res
+func joinChannels(chs ...<-chan int) chan int {
+	res := make(chan int)
+
+	wg := &sync.WaitGroup{}
+
+	for _, v := range chs {
+		wg.Go(func() {
+			for val := range <-v {
+				res <- val
+			}
+		})
+	}
+
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+
+	return res
 }
-
-
-
-
